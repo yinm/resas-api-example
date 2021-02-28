4365001f33dec58f6f2fc8d30be2c2132de1fd17
yinm <yinm.cminor9@gmail.com>
2021-02-28 14:19:37 +0900

    スタイルを当てる

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
-  <h1>都道府県別の総人口推移グラフ</h1>
-  <div>
-    <h2>都道府県</h2>
-    <div>
-      <div v-for="prefecture in prefectures" :key="prefecture.prefCode">
-        <input
-          :id="prefecture.prefName"
-          type="checkbox"
-          name="prefCode"
-          :value="prefecture.prefCode"
-        />
-        <label :for="prefecture.prefName">{{ prefecture.prefName }}</label>
+  <div class="stack-5 center">
+    <h1>都道府県別の総人口推移グラフ</h1>
+    <div class="stack-3">
+      <h2>都道府県</h2>
+      <div class="App-checkboxes">
+        <div v-for="prefecture in prefectures" :key="prefecture.prefCode">
+          <input
+            :id="prefecture.prefName"
+            type="checkbox"
+            name="prefCode"
+            :value="prefecture.prefCode"
+          />
+          <label :for="prefecture.prefName">{{ prefecture.prefName }}</label>
+        </div>
       </div>
     </div>
   </div>
@@ -31,4 +33,14 @@ export default defineComponent({
 });
 </script>
 
-<style></style>
+<style>
+@import "./assets/stylesheets/index.css";
+</style>
+
+<style scoped>
+.App-checkboxes {
+  display: flex;
+  flex-wrap: wrap;
+  gap: var(--space-4);
+}
+</style>
