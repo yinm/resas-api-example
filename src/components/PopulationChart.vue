33e2ad8ac9719f5f2a73a22cd5762eb0a61e9cb2
yinm <yinm.cminor9@gmail.com>
2021-02-28 14:38:27 +0900

    総人口推移グラフを表示するためのコンポーネントを作る

---
+++ src/components/PopulationChart.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="chart" />
+</template>
+
+<script lang="ts">
+import { defineComponent, computed, watch, onMounted } from "vue";
+import type { PropType } from "vue";
+import Highcharts from "highcharts";
+import type { SeriesLineOptions } from "highcharts";
+
+export default defineComponent({
+  props: {
+    series: {
+      type: Array as PropType<SeriesLineOptions[]>,
+      required: true,
+    },
+  },
+
+  setup(props) {
+    const chartOptions = computed(() => {
+      return {
+        chart: {
+          renderTo: "chart",
+        },
+        title: {
+          text: "総人口",
+        },
+        series: props.series,
+        xAxis: {
+          title: {
+            text: "年度",
+          },
+        },
+        yAxis: {
+          title: {
+            text: "人口数",
+          },
+        },
+      };
+    });
+
+    onMounted(() => {
+      Highcharts.chart(chartOptions.value);
+    });
+
+    watch(chartOptions, (newValue) => {
+      Highcharts.chart(newValue);
+    });
+  },
+});
+</script>
