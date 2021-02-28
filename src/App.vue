b822d86ef30513296d7f31149c929cbb11660e50
yinm <yinm.cminor9@gmail.com>
2021-02-28 14:09:45 +0900

    APIから取得した都道府県のデータで、チェックボックスを生成する

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,34 @@
-<template></template>
+<template>
+  <h1>都道府県別の総人口推移グラフ</h1>
+  <div>
+    <h2>都道府県</h2>
+    <div>
+      <div v-for="prefecture in prefectures" :key="prefecture.prefCode">
+        <input
+          :id="prefecture.prefName"
+          type="checkbox"
+          name="prefCode"
+          :value="prefecture.prefCode"
+        />
+        <label :for="prefecture.prefName">{{ prefecture.prefName }}</label>
+      </div>
+    </div>
+  </div>
+</template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import { usePrefectures } from "@/composables/use-prefectures";
 
-export default defineComponent({});
+export default defineComponent({
+  setup() {
+    const { prefectures } = usePrefectures();
+
+    return {
+      prefectures,
+    };
+  },
+});
 </script>
 
 <style></style>
